6c7624b2e3970e43bff09e6ca2c2f1e2e4d55e7e
nkubana0 <i.shema@alustudent.com>
2023-11-01 01:58:54 -0700

    gather from api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+"""Module"""
+
 import requests
 import sys
 
-# Get data from the JSON URL
-# Give an error message incase the urls are not available
-# Calculate the number of completed and total tasks
-# Display employee todo list progress
-
+""" Module"""
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
 
