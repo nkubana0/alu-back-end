f694616f0e5495cb7888799532fa3d2e95a247cd
nkubana0 <i.shema@alustudent.com>
2023-11-04 05:16:49 -0700

    gather from api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,27 +7,36 @@ import sys
 
 
 def get_employee_todo_list_progress(employee_id):
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response = requests.get(url)
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{base_url}/users/{employee_id}'
+    response = requests.get(user_url)
+
     if response.status_code == 200:
         user_data = response.json()
         employee_name = user_data['name']
-        tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+
+        tasks_url = f'{base_url}/todos?userId={employee_id}'
         tasks_response = requests.get(tasks_url)
+
         if tasks_response.status_code == 200:
             tasks_data = tasks_response.json()
             completed_tasks = [
-                task for task in tasks_data if task['completed']]
+                task for task in tasks_data if task['completed']
+            ]
             num_completed_tasks = len(completed_tasks)
             total_num_tasks = len(tasks_data)
-            print(
-                f'Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):')
+
+            print(f'Employee {employee_name} is done with tasks '
+                  f'({num_completed_tasks}/{total_num_tasks}):')
+
             for task in completed_tasks:
                 print(f'\t {task["title"]}')
         else:
-            print(f'Failed to retrieve tasks data for employee ID {employee_id}')
+            print(
+                f'Failed to retrieve tasks data for employee ID {employee_id}')
     else:
         print(f'Failed to retrieve user data for employee ID {employee_id}')
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
