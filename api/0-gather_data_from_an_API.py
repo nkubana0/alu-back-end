31d68ccaa5707e082451225c48e2401a6cb53151
nkubana0 <i.shema@alustudent.com>
2023-11-04 10:27:13 -0700

    gather from api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,39 +1,27 @@
 #!/usr/bin/python3
-
 """
-GET from API and return employee todo progress when given the employee Id
+Request from API; Return TODO list progress given employee ID
 """
-
 import requests
-import sys
+from sys import argv
 
 
 def get_employee_todo_list_progress(employee_id):
-    """
-    GET the API data
-    """
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    response = requests.get(user_url)
-
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(url)
     if response.status_code == 200:
         user_data = response.json()
         employee_name = user_data['name']
-
-        tasks_url = f'{base_url}/todos?userId={employee_id}'
+        tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
         tasks_response = requests.get(tasks_url)
-
         if tasks_response.status_code == 200:
             tasks_data = tasks_response.json()
             completed_tasks = [
-                task for task in tasks_data if task['completed']
-            ]
+                task for task in tasks_data if task['completed']]
             num_completed_tasks = len(completed_tasks)
             total_num_tasks = len(tasks_data)
-
-            print(f'Employee {employee_name} is done with tasks '
-                  f'({num_completed_tasks}/{total_num_tasks}):')
-
+            print(
+                f'Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_num_tasks}):')
             for task in completed_tasks:
                 print(f'\t {task["title"]}')
         else:
